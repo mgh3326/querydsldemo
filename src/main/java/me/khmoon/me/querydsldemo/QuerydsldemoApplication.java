6635345ff311ab91b7207287914f1199a08f5433
kwanghyunMoon <mgh3326@naver.com>
2019-07-24 15:35:39 +0900

    first update

---
+++ src/main/java/me/khmoon/me/querydsldemo/QuerydsldemoApplication.java
@@ -0,0 +1,13 @@
+package me.khmoon.me.querydsldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydsldemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuerydsldemoApplication.class, args);
+  }
+
+}
